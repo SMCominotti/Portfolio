b85e4a18a6fcb3f9ebcc63914b31a2ded805a0a8
SMCominotti <smcominotti@gmail.com>
2023-08-12 00:45:29 -0300

    Particles

--- pages/index.js
+++ pages/index.js
@@ -51,11 +51,17 @@ className="hidden xl:flex xl:ml-80"
 </div>
 <div className="w-[1200px] h-full absolute right-0 bottom-0">
 <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
-<div>Particles</div>
+<ParticlesContainer/>
 <div>
-<div className="w-full h-full max-w-[537px] max-h-[478px] absolute bottom-6 lg:bottom-12 lg:right-[3%]">
+<motion.div 
+variants={fadeIn("up", 0.5)}
+initial="hidden"
+animate="show"
+exit="hidden"
+transition={{duration:1, ease: 'easeInOut'}}
+className="w-full h-full max-w-[537px] max-h-[478px] absolute bottom-6 lg:bottom-12 lg:right-[3%]">
   <Avatar/>
-</div>
+</motion.div>
 </div>
 </div>
 </div>
