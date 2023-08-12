5cf81141c3e02a6644248a7697661f0c31b1838d
SMCominotti <smcominotti@gmail.com>
2023-08-11 23:43:48 -0300

    foto

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,5 +1,17 @@
+import Image from 'next/image';
+
 const Avatar = () => {
-  return <div>Avatar</div>;
+  return (
+  <div className="hidden xl:flex xl:max-w-none">
+    <Image
+    src={'/avatar5.png'}
+    width={537}
+    height={478}
+    alt=''
+    className="translate-z-0 w-full h-full"
+    /> 
+    </div>
+  );
 };
 
 export default Avatar;
