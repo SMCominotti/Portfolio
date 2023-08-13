8126d92909ce3335ce4cd540e3b1ce2041ccfe12
SMCominotti <smcominotti@gmail.com>
2023-08-13 20:13:35 -0300

    logo

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,27 @@
+import Image from 'next/image';
+import Link from 'next/Link';
+import Socials from '../components/Socials'
+
 const Header = () => {
-  return <header className="absolute z-30 w-full">Header</header>;
+  return (
+    <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]">
+      <div className='container mx-auto'>
+        <div className="mt-20 ml-[-330px]"> 
+          <Link href={'/'}>
+            <Image
+              src="/logs.svg"
+              width={628}
+              height={38}
+              alt=""
+              priority={true}
+            />
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
+
+
--- pages/index.js
+++ pages/index.js
@@ -51,7 +51,7 @@ className="hidden xl:flex xl:ml-80"
 </div>
 <div className="w-[1200px] h-full absolute right-0 bottom-0">
 <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
-<ParticlesContainer/>
+{/* <ParticlesContainer/> */}
 <div>
 <motion.div 
 variants={fadeIn("up", 0.5)}
