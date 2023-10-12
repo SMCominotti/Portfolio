3b73823814cf08f9bfc902044b3c780429491bf9
SMCominotti <smcominotti@gmail.com>
2023-10-12 17:42:32 -0300

    contact

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -8,6 +8,23 @@ import { motion } from "framer-motion";
 //variants
 import { fadeIn } from "../../variants";
 
+if (typeof window !== "undefined") {
+  const $form = document.querySelector('#form');
+  const $buttonMailto = document.querySelector('#mail');
+
+
+  $form.addEventListener('submit', handleSubmit);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const form = new FormData(this);
+
+    $buttonMailto.setAttribute('href', `mailto:smcominotti@gmail.com?subject=Nombre: ${form.get('name')} Correo: ${form.get('email')} Asunto: ${form.get('subject')}&body=${form.get('message')}`);
+$buttonMailto.click();
+
+  }
+}
+
 const Contact = () => {
   return (
     <div className="h-full bg-primary/30 py-32 text-center xl:text-left">
@@ -40,27 +57,29 @@ const Contact = () => {
            initial='hidden'
            animate='show'
            exit='hidden'
-          className="flex-1 flex flex-col gap-6 w-full mx-auto">
+          className="flex-1 flex flex-col gap-6 w-full mx-auto"
+          id="form" action="">
             <div className="flex gap-x-6 w-full">
               <div className="flex flex-col w-1/2">
-                <input type="text" placeholder="Nombre" className="input" />
+                <input type="text" name="name" id="name" required placeholder="Nombre" className="input" />
               </div>
               <div className="flex flex-col w-1/2">
-                <input type="text" placeholder="Email" className="input" />
+                <input type="text" name="email" id="email" required placeholder="Email" className="input" />
               </div>
             </div>
             <div className="flex flex-col w-full">
-              <input type="text" placeholder="Asunto" className="input" />
+              <input type="text" required placeholder="Asunto" className="input" name="subject" id="subject"/>
             </div>
             <div className="flex flex-col w-full">
-              <textarea placeholder="Mensaje" className="textarea"></textarea>
+              <textarea name="message" id="message" required placeholder="Mensaje" className="textarea"></textarea>
             </div>
-            <button className="btn rounded-full border border-white/50 max-w-[170] px-20 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group">
+            <button type="submit" className="btn rounded-full border border-white/50 max-w-[170] px-20 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group">
              <span className="group-hover:-translate-y-[10%] group-hover:opacity-50 hover:text-purple-200 transition-all duration-500">
                 Hablemos
               </span>
             </button>
           </motion.form>
+          <a href="mailto:smcominotti@gmail.com" id="mail" className="opacity-0">mail</a>
         </div>
       </div>
     </div>
