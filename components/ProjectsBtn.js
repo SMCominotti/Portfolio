fa88e4486c5a260e5320c3a62d39583c211a1668
SMCominotti <smcominotti@gmail.com>
2023-10-19 15:10:49 -0300

    update

--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -1,21 +1,24 @@
 import Image from "next/image";
-import Link from "next/link";
-import {HiArrowRight} from 'react-icons/hi2'
+import {LiaTelegramPlane} from 'react-icons/lia'
 
 const ProjectsBtn = () => {
-  return <div className="mx-auto xl:mx-2">
-    <Link href={'/work'} className="relative w-[120px] h-[120px] flex justify-center items-center  bg-cover bg-center bg-no-repeat group">
+
+  return (
+  <div className="mx-auto xl:mx-2">
+    <div 
+    className="relative w-[200px] h-[200px] flex justify-center items-center  bg-cover bg-center bg-no-repeat group " 
+    >
       <Image 
-      src={'/rounded-text.png'} 
-      width={101} 
-      height={108} 
+      src={'/DisfrutaBco.png'} 
+      width={150} 
+      height={158} 
       alt='' 
-      className="animate-spin-slow w-full h-full max-w-[91px] max-h-[98px]"
+      className="animate-spin-slow w-full h-full max-w-[110px] max-h-[110px] cursor-pointer"
       />
-      <HiArrowRight className="absolute text-4xl group-hover:translate-x-2 transition-all duration-300"/>
-    </Link>
-    
-    </div>;
-};
+      <LiaTelegramPlane className="absolute text-4xl justify-center group-hover:translate-x-2 transition-all duration-300"/>
+    </div>
+  </div>
+  );
+}
 
 export default ProjectsBtn;
--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -97,15 +97,15 @@ const WorkSlider = () => {
             <div className="grid grid-cols-2 grid.rows-2 gap-4 cursor-pointer">
               {slide.images.map((image, index) => {
                 return (
-                  <div className="relative rounded-lg overflow-hidden flex items-center justify-center group">
-                    <div className="flex items-center justify-center relative overflow-hidden group" 
-                    key={index}>
-                      {/* image */}
-                      <Image src={image.path} width={500} height={300} alt=''/>
-                      {/* overlay gradiente */}
-                      <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700"></div>
-                      {/* title */}
-                      <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300 md:group-hover:-translate-y-20 md:group-hover:xl:-translate-y-30">
+                  <div className="relative rounded-lg overflow-hidden flex items-center justify-center group" key={index}>
+                  <div className="flex items-center justify-center relative overflow-hidden group">
+                    {/* image */}
+                    <Image src={image.path} width={500} height={300} alt=''/>
+                    {/* overlay gradiente */}
+                    <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700"></div>
+                    {/* title */}
+                    <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300 md:group-hover:-translate-y-20 md:group-hover:xl:-translate-y-30">
+                      <a href={image.url} target="_blank" rel="noopener noreferrer" className="group">
                         <div className="flex items-center gap-x-2 text-[13px] tracking-[0.2em]">
                           {/* title part 1 */}
                           <div className="delay-100">VER</div>
@@ -114,9 +114,11 @@ const WorkSlider = () => {
                           {/* icon */}
                           <div className="text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200"><BsArrowRight/></div>
                         </div>
-                      </div>
-                      </div>
+                      </a>
+                    </div>
                   </div>
+                </div>
+                
                 );
               })}
             </div>
