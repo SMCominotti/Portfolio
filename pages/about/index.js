dde9b634e9c8379a7dc3cabba5e29dc9d41b5d6a
SMCominotti <smcominotti@gmail.com>
2024-01-31 16:04:26 +0100

    prueba

--- pages/about/index.js
+++ pages/about/index.js
@@ -37,7 +37,7 @@ export const aboutData = [
       {
         title: "UX/UI Design",
         icons: [<FaFigma key="Figma"/>],
-         [
+         title:[
           "User Research",
           "Information Architecture",
           "User Interface (UI) Design",
