621cde0d11f70a67d3b86e8e078639ea6ef895e9
SMCominotti <smcominotti@gmail.com>
2024-02-06 21:44:27 +0100

    gitHub and Linkedin in new windows

--- components/Socials.js
+++ components/Socials.js
@@ -7,15 +7,16 @@ import {
 
 const Socials = () => {
   return (
-  <div className="flex items-center gap-x-5 text-lg text-white/60">
-    <Link href={'https://github.com/SMCominotti'} className="hover:text-accent transition-all duration-300">
-      <AiFillGithub/>
-    </Link>
-    <Link href={'https://www.linkedin.com/in/stella-maris-cominotti/'} className="hover:text-accent transition-all duration-300">
-      <AiFillLinkedin/>
-    </Link>
-  </div>
+    <div className="flex items-center gap-x-5 text-lg text-white/60">
+      <a href="https://github.com/SMCominotti" target="_blank" rel="noopener noreferrer" className="hover:text-accent transition-all duration-300">
+        <AiFillGithub/>
+      </a>
+      <a href="https://www.linkedin.com/in/stella-maris-cominotti/" target="_blank" rel="noopener noreferrer" className="hover:text-accent transition-all duration-300">
+        <AiFillLinkedin/>
+      </a>
+    </div>
   );
 };
 
 export default Socials;
+
